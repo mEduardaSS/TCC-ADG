9514b730efb67142a51bbe856b62cd200ff6c017
mEduardaSS <mariaeduardasouzasantos423@gmail.com>
2023-08-17 23:06:10 -0300

    Iniciando TCC

---
+++ TCC-AbrigoDosGatos/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TCC-AbrigoDosGatos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
