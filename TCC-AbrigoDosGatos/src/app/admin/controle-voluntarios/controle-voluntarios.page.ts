edbc0873851321d10227316630584468fe623ebc
Gabriela Pantaleão <95754197+GabrielaPantaleao@users.noreply.github.com>
2023-10-10 15:27:49 +0000

    mudanças do controle de voluntários

--- TCC-AbrigoDosGatos/src/app/admin/controle-voluntarios/controle-voluntarios.page.ts
+++ TCC-AbrigoDosGatos/src/app/admin/controle-voluntarios/controle-voluntarios.page.ts
@@ -9,7 +9,21 @@ export class ControleVoluntariosPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    console.log("oi");
+    console.log( await this.pegaDadosAPI());
+    console.log("oi");
+
+  }
+
+  async pegaDadosAPI(){
+	const url = 'const url = `http://aula/API/get';
+
+	await fetch(url)
+	.then(response => response.json())
+	.then((dados) => { console.log(dados) })
+	.catch((_) => { console.log(_) })
+
   }
 
 }
